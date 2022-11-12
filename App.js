2ea7cc2323e9acc39e0cfe4a8e76817109efddf2
Pharaoh-Omid <61551786+Pharaoh-Omid@users.noreply.github.com>
2022-11-12 17:23:53 +0330

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react';
+import Data from './components/Data';
+const App = () => {
+ 
+  return (
+    <div>
+      <Data />
+    </div>
+  );
+};
+
+export default App;
---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {BrowserRouter} from 'react-router-dom'
+import App from './App';
+import ReactDOM from "react-dom";
+ReactDOM.render(
+<BrowserRouter>
+<App /></BrowserRouter>, document.getElementById("root"));
+
+
